f50b19c2a8edb941033d40b2209504e378d1e4ff
Faisal Ridwan <faisal.intern@stoqo.com>
2020-08-21 19:26:48 +0700

    Add About us section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import Header from "./components/Header";
+import AboutUs from "./components/AboutUs";
 
 function App() {
+  const [isFirstOpen, setIsFirstOpen] = useState(true);
+
+  useEffect(() => {
+    if (isFirstOpen) setIsFirstOpen(false);
+  }, [isFirstOpen]);
+
   return (
     <div>
       <Header />
-      <div></div>
+      <AboutUs />
     </div>
   );
 }
---
+++ src/components/AboutUs/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+function AboutUs() {
+  return (
+    <div id="about-container">
+      <div className="sub-title" style={{ fontWeight: "bold" }}>
+        About Us
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
