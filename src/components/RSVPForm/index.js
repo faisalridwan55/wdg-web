801ead05cef059c7ad4ef7a8969a1e4d1abd22fd
faisal <faisalridwan55@gmail.com>
2020-09-08 23:50:35 +0700

    Progress of RSVP form

--- src/components/RSVPForm/index.js
+++ src/components/RSVPForm/index.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import axios from 'axios';
+
 import "./index.css";
 
 function RSVPForm() {
@@ -18,9 +20,11 @@ function RSVPForm() {
   const [formData, setFormData] = useState({
     name: "",
     phone: "",
-    guestOf: "Laras",
+    guestOf: "Faisal",
     presence: true,
   });
+
+  const postForm = () => axios.post(process.env.REACT_APP_FORM_URL, formData);
 
   return (
     <>
@@ -84,7 +88,7 @@ function RSVPForm() {
               </div>
             </div>
           </div>
-          <div className="my-col my-3 mb-5 px-5">
+          <div className="my-col my-3 px-5">
             <div style={formTitle}>Will you come?</div>
             <div className="row justify-content-center align-items-center">
               <div className="text-align-center mx-2">
@@ -115,6 +119,9 @@ function RSVPForm() {
                 Unfortunately I can't
               </div>
             </div>
+          </div>
+          <div className="mb-5 text-center">
+            <button className="btn btn-success w-50" onClick={postForm}>Submit</button>
           </div>
         </div>
       </div>
